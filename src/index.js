d28cd849ff7c32da88a57c2842542aef36d6a6ff
Ernest Bagretsov <kumadeji@yandex.ru>
2021-07-27 12:29:43 +0300

    a

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ function onTransitionEnd() {
 }
 
 function declOfNum(number, words) {  
-    return number +' '+ words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
+    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
 }
 
 function getSmartCat() {
