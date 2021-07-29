06a9db21bdfda2199382585a9466abe05bd40264
Ernest Bagretsov <kumadeji@yandex.ru>
2021-07-29 13:24:10 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "/sass/main.scss";
 
 // preloader
 const preloaderTL = gsap.timeline();
-preloaderTL.to('#logo', {yPercent: -20, opacity: 0, delay: 4})
+preloaderTL.to('#preloader-text-main', {yPercent: -20, opacity: 0, delay: 4})
+preloaderTL.to('#preloader-text-over', {yPercent: -20, opacity: 0, delay: 4})
+preloaderTL.to('#preloader-text-under', {yPercent: -20, opacity: 0, delay: 4})
 preloaderTL.to('.preloader', {transform: 'scaleY(0)', transformOrigin: 'top', delay: 1})
 
 // custom cursor
