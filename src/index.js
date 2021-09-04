f88c6955e28e2d16074ffc80914d00a8f043a07b
Ernest Bagretsov <kumadeji@yandex.ru>
2021-09-05 02:14:46 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -123,7 +123,7 @@ const timer = 6000;
 
 function getQuotes() {
   fetch(
-    "https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json"
+    "../quotes.json"
   )
     .then((response) => response.json())
     .then((data) => setInterval(() => shuffleQuotes(data.data), timer));
