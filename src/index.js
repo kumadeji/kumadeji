49ee0b9359eeb1b87dec79d9a44ead3a4bcf47d3
Ernest Bagretsov <kumadeji@yandex.ru>
2021-09-29 22:49:43 +1000

    changed jsonp proxy

--- src/index.js
+++ src/index.js
@@ -168,7 +168,8 @@ function declOfNum(number, words) {
 }
 
 function getSmartCat() {
-  fetch("https://jsonp.afeld.me/?callback=&url=https%3A%2F%2Fru.smartcat.com%2Fproxycat%2Fapi%2Ffreelancers%2Fprofile%2Fernest-bagretsov")
+  // fetch("https://jsonp.afeld.me/?callback=&url=https%3A%2F%2Fru.smartcat.com%2Fproxycat%2Fapi%2Ffreelancers%2Fprofile%2Fernest-bagretsov")
+  fetch("https://norigin.herokuapp.com/?url=https://ru.smartcat.com/proxycat/api/freelancers/profile/ernest-bagretsov")
   .then((response) => {
     return response.json();
   })
