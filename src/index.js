7988794d2eae5783f71fa04afcb970464cf3f814
Ernest Bagretsov <kumadeji@yandex.ru>
2022-08-09 20:51:19 +1000

    content update

--- src/index.js
+++ src/index.js
@@ -121,9 +121,9 @@ const quote = document.querySelector("q");
 const timer = 6000;
 
 function getQuotes() {
-  fetch(
-    "https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json"
-  )
+  // fetch("https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json")
+  fetch("https://raw.githubusercontent.com/kumadeji/kumadeji/master/quotes.json")
+  
     .then((response) => response.json())
     .then((data) => setInterval(() => shuffleQuotes(data.data), timer));
 }
